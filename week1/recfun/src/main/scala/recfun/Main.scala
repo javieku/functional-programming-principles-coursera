a606c9a4cca3d4de0fd3036658a6a3493fd84266
javieku <javieku@gmail.com>
2018-03-26 20:22:02 +0200

    Solve exercise 2

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -25,7 +25,25 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean =
+    {
+      def balance_impl(acc: String, chars: List[Char]): Boolean =
+      {
+        if (chars.isEmpty)
+          acc.isEmpty
+        else if (chars.head.equals('('))
+          balance_impl(acc + "(", chars.tail)
+        else if (chars.head.equals(')'))
+          if(acc.isEmpty)
+            false
+          else
+            balance_impl(acc.dropRight(1), chars.tail)
+        else
+          balance_impl(acc, chars.tail)
+      }
+
+      balance_impl("", chars)
+    }
   
   /**
    * Exercise 3
